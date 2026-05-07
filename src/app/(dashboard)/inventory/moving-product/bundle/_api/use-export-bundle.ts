4d73244adf0e7ce03c0c7c39644591e3a38ae06e
lusipr <lusipr142@gmail.com>
2026-05-07 17:19:40 +0700

    fixs export bundle

--- src/app/(dashboard)/inventory/moving-product/bundle/_api/use-export-bundle.ts
+++ src/app/(dashboard)/inventory/moving-product/bundle/_api/use-export-bundle.ts
@@ -7,18 +7,25 @@ import { getCookie } from "cookies-next/client";
 
 type Error = AxiosError;
 
-export const useExportBundle = () => {
+export const useExportBundle = ({ start_date, end_date }: any) => {
   const accessToken = getCookie("accessToken");
 
   const mutation = useMutation<AxiosResponse, Error, "">({
     mutationFn: async () => {
+      const query = new URLSearchParams();
+
+      if (start_date) query.append("start_date", start_date);
+      if (end_date) query.append("end_date", end_date);
+
       const res = await axios.get(
-        `${baseUrl}/exportBundles`,
+        `${baseUrl}/exportBundles${
+          query.toString() ? `?${query.toString()}` : ""
+        }`,
         {
           headers: {
             Authorization: `Bearer ${accessToken}`,
           },
-        }
+        },
       );
       return res;
     },
