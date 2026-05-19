5d92c8db682cb38790b2f2093c148aefa3139fe0
lusipr <lusipr142@gmail.com>
2026-05-19 17:35:22 +0700

    add history in detail check history

---
+++ src/app/(dashboard)/inbound/check-history/[historyId]/history/_api/use-export-history.ts
@@ -0,0 +1,40 @@
+import { useMutation } from "@tanstack/react-query";
+import axios, { AxiosError } from "axios";
+import type { AxiosResponse } from "axios";
+import { baseUrl } from "@/lib/baseUrl";
+import { toast } from "sonner";
+import { getCookie } from "cookies-next/client";
+
+type Error = AxiosError;
+
+export const useExportHistory = (id: string) => {
+  const accessToken = getCookie("accessToken");
+
+  const mutation = useMutation<AxiosResponse, Error, "">({
+    mutationFn: async () => {
+      const res = await axios.get(
+        `${baseUrl}/history-edit-products/export/${id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        },
+      );
+      return res;
+    },
+    onSuccess: () => {
+      toast.success("File Successfully Exported");
+    },
+    onError: (err) => {
+      if (err.status === 403) {
+        toast.error(`Error 403: Restricted Access`);
+      } else {
+        toast.error(
+          `ERROR ${err?.status}: History failed to export`,
+        );
+        console.log("ERROR_EXPORT_HISTORY:", err);
+      }
+    },
+  });
+  return mutation;
+};
